8a778c13cf3807886cee01a19933b8e59672d0b1
IOUnix <TLColeman86@gmail.com>
2026-03-02 11:40:02 -0800

    Fully working start and game over screen. Next I just need to add the character selector and ads and the we'll be all set.

--- Birds_of_a_feather/Assets/Scripts/GameManager.cs
+++ Birds_of_a_feather/Assets/Scripts/GameManager.cs
@@ -8,6 +8,31 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     [SerializeField] private GameObject _gameOverCanvas;
+    [SerializeField] private GameObject startScreen;
+    [SerializeField] private GameObject gameplayContainer;
+    [SerializeField] private GameObject gameOverScreen;
+
+    private static bool _skipStartScreenOnce = false;
+
+    private void Start()
+    {
+        if (_skipStartScreenOnce)
+        {
+            _skipStartScreenOnce = false;
+
+            startScreen.SetActive(false);
+            gameOverScreen.SetActive(false);
+            gameplayContainer.SetActive(true);
+
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            startScreen.SetActive(true);
+            gameplayContainer.SetActive(false);
+        }
+    }
 
     private void Awake()
     {
@@ -15,7 +40,13 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
+    }
 
+    public void Play()
+    {
+        startScreen.SetActive(false);
+        gameOverScreen.SetActive(false);
+        gameplayContainer.SetActive(true);
         Time.timeScale = 1f;
     }
 
@@ -25,9 +56,10 @@ public class GameManager : MonoBehaviour
 
         Time.timeScale = 0f;
     }
-
     public void RestartGame()
     {
+        _skipStartScreenOnce = true;
+        Time.timeScale = 1f; // important
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
